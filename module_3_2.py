22cf96a2e4ee4fb38d115f4930d36c0763bfaf31
OlgaSysoeva-git <sisoeva2504@gmail.com>
2024-11-29 14:03:47 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,38 @@
+from logging.config import valid_ident
+
+
+def send_email(message, recipient,*, sender= "university.help@gmail.com"):
+    valid_email = recipient.find('@') >= 0 and recipient.endswith((".com", ".ru", ".net")),\
+            sender.find('@') >= 0 and sender.endswith((".com", ".ru", ".net"))
+
+    if valid_email == False:
+         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+         return
+
+    if sender == recipient :
+         print('Нельзя отправить письмо самому себе!')
+         return
+
+    if sender == 'university.help@gmail.com':
+         print(f'"Письмо успешно отправлено с адреса {sender} на адрес {recipient}."')
+    else:
+         print(f'"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} \
+         на адрес {recipient}."')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
+
+
+
+
+
+
+
+
+
+
+
